5245edc906780037f0ad6df569eeb97b49c34f5f
ynchoe <125226207+ynchoe@users.noreply.github.com>
2023-03-09 16:29:43 +0900

    꾸미기 전 기본 구현

---
+++ script.js
@@ -0,0 +1,48 @@
+// index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
+console.log(agoraStatesDiscussions);
+
+// convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
+const convertToDiscussion = (obj) => {
+  const li = document.createElement("li"); // li 요소 생성
+  li.className = "discussion__container"; // 클래스 이름 지정
+
+  const avatarWrapper = document.createElement("div");
+  avatarWrapper.className = "discussion__avatar--wrapper";
+  const discussionContent = document.createElement("div");
+  discussionContent.className = "discussion__content";
+  const discussionAnswered = document.createElement("div");
+  discussionAnswered.className = "discussion__answered";
+
+  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  avatarWrapper.innerHTML = `<img class="discussion__avatar--image"
+  src="${obj.avatarUrl}"
+  alt="avatar of ${obj.author}">`
+
+  discussionContent.innerHTML = `<h2 class="discussion__title"><a
+  href=${obj.url}>${obj.title}</a></h2>
+  <div class="discussion__information">${obj.author} / ${obj.createdAt}</div>
+  </div>
+  `
+
+  if (obj.answer !== null) {
+    discussionAnswered.innerHTML = `<p>✅</p>`
+  } else {
+    discussionAnswered.innerHTML = `<p>❌</p>`
+  }
+
+
+  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  return li;
+};
+
+// agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+const render = (element) => {
+  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+    element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+  }
+  return;
+};
+
+// ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
+const ul = document.querySelector("ul.discussions__container");
+render(ul);
